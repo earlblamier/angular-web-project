49f7e654fa5a90a0f057f0af9c08f9e568641a39
Earl Lamier <earlblamier@gmail.com>
2025-02-27 19:43:37 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { IconGridComponent } from './icon-grid/icon-grid.component';
+import { CardComponent } from './card/card.component';
+import { MainContentComponent } from "./main-content/main-content.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, IconGridComponent, CardComponent, MainContentComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'] // Corrected property name
+})
+export class AppComponent {
+  title = 'tazkify';
+}
---
+++ src/app/icon-grid/icon-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-grid',
+  imports: [],
+  templateUrl: './icon-grid.component.html',
+  styleUrl: './icon-grid.component.scss'
+})
+export class IconGridComponent {
+
+}
