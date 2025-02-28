469ceb7e6b931951d2f3bd5a3b146b862f4a5e4f
Earl Lamier <earlblamier@gmail.com>
2025-02-28 00:15:07 -0500

    add new ad image updated footer and new main

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
